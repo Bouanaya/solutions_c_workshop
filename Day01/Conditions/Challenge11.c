7251f6083734eb7feeae48a268fccfc848d7c3f7
Bouanaya <bouanayasoufiane@gmail.com>
2024-09-25 20:15:48 +0200

    Challenge11

--- Day01/Conditions/Challenge11.c
+++ Day01/Conditions/Challenge11.c
@@ -1,9 +1,40 @@
  #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+#include <stdlib.h>  
+#include <time.h>    
 
-int main() {
- 
+int main() { 
+    
+    srand(time(NULL));
+int go = rand() ;
+printf("%d",go) ;
+
+
+    int tim = rand() % 7;
+    switch (tim)
+    {
+    case 0 : printf("lundi");  
+        break;
+    case 1 : printf("mardi");
+        break;
+         case 2 : printf("mercredi");
+        break;
+         case 3 : printf("jeudi");
+        break;
+         case 4 : printf("vendredi");
+        break;
+          case 5 : printf("samedi");
+        break;
+          case 6 : printf("dimanche");
+        break;
+
+    }
+    
+
+   
+   
+
+    
+    
 
     return 0;
 }
