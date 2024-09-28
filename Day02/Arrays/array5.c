72e3ce5167f8de413fe6e4efc362e1c7a4572de7
Bouanaya <bouanayasoufiane@gmail.com>
2024-09-28 16:45:55 +0200

    Challenge 5

---
+++ Day02/Arrays/array5.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, i, min;
+
+    printf("Entrez le nombre d'éléments: ");
+    scanf("%d", &n);
+
+    int tab[n];
+
+    for (i = 0; i < n; i++)
+    {
+        printf("Entrez l'élément %d: ", i + 1);
+        scanf("%d", &tab[i]);
+    }
+
+    min = tab[0];
+
+    for (i = 1; i < n; i++)
+    {
+        if (tab[i] < min)
+        {
+            min = tab[i];
+        }
+    }
+
+    printf("Le plus petit élément est: %d\n", min);
+
+    return 0;
+}
